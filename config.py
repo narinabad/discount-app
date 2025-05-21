4083fee0af47450e5508a2ea676df576b0fb3fbc
maryam najafi <maryam.najafi733@.com>
2025-05-21 16:44:55 +0330

    fix environment variable names for railway

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
-SECRET_KEY='hsudyf.,mjhuygsauhipoo//.,'
-PORT=8080
-MYSQL_CONFIG = "mysql+mysqlconnector://root:xEgLoIwXkUjNIIbUuSakgsOReHNfAUOG@mysql.railway.internal:3306/railway"
+import os
 
+SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret")
+PORT = int(os.environ.get("PORT", 8080))
+
+SQLALCHEMY_DATABASE_URI = (
+    f"mysql+mysqlconnector://{os.environ['MYSQLUSER']}:{os.environ['MYSQLPASSWORD']}"
+    f"@{os.environ['MYSQLHOST']}:{os.environ['MYSQLPORT']}/{os.environ['MYSQLDATABASE']}"
+)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
