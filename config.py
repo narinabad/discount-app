baece71f802cca8cf14746115b531110374f91e5
maryam najafi <maryam.najafi733@.com>
2025-05-21 17:19:23 +0330

    Configure  MySQLl

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
-SECRET_KEY='hsudyf.,mjhuygsauhipoo//.,'
-PORT=8080
-MYSQL_CONFIG = "mysql+mysqlconnector://root:xEgLoIwXkUjNIIbUuSakgsOReHNfAUOG@mysql.railway.internal:3306/railway"
-
 import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret")
+PORT = int(os.environ.get("PORT", 8080))
+
 SQLALCHEMY_DATABASE_URI = (
-    f"mysql+mysqlconnector://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@"
-    f"{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}"
+    f"mysql+mysqlconnector://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}"
+    f"@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}"
 )
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
